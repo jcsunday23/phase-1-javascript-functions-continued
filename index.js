744951d28aed6e8c040e49ffc1758f4dc4ffb628
Chris Jesse <jessechris23@gmail.com>
2025-05-01 18:36:24 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(adj = 'dedicated') {
+        return `You are ${flair}${adj}${flair}!`;
+    };
+}
+saturdayFun();
+saturdayFun("playing-dut");
+mondayWork();
+mondayWork("charing-meeting");
+const encouragingPromptFunction=wrapAdjective("programmer");
+console.log(encouragingPromptFunction)
